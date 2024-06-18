3049ecbfc30ff1bc1d06ffff6798063c4a13f68f
Suchayan Chatterjee <suchayanchatterjee@gmail.com>
2024-06-18 21:51:31 +0300

    initial commit

---
+++ src/main/java/com/hello_cloud/hello_rest/HelloRestApplication.java
@@ -0,0 +1,13 @@
+package com.hello_cloud.hello_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloRestApplication.class, args);
+	}
+
+}
